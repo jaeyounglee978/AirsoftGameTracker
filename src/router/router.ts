78394932d49f0f9640580406593bbb09085ace11
jaeyounglee <jaeyoung.lee@navercorp.com>
2020-07-14 11:13:47 +0900

    저장

--- src/router/router.ts
+++ src/router/router.ts
@@ -6,7 +6,7 @@ import * as test_controller from '../controller/test_controller'
 
 router.get('/top', test_controller.top)
 
-router.get('/chat/new-chat', test_controller.makeNewChat)
+router.get('/chat/new-chat', test_controller.makeNewGameRoom)
 router.get('/chat/participate-chat', test_controller.participateChat)
 router.get('/chat/:chatroomId', test_controller.chat)
 
